5485f0a1783f365bbef621ebe9eeb921d9f17a64
fuchrie <LuoXuan_Mmys@hotmail.com>
2025-03-17 22:01:02 +0800

    Initial commit

--- try.sh
+++ try.sh
@@ -1,12 +1,13 @@
 git pull
 npm config set registry https://registry.npmmirror.com
 npm cache clean --force
-npm install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # 加载 nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # 加载 nvm 的 bash 补全功能
 nvm install 20.11.1
+nvm use 20.11.1
+npm install
 npm run build
 docker stop biotool
 docker rm -f biotool
